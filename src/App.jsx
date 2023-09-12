276622e123277723bf3f34f365a6b73ebaa56e19
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-09-12 22:31:59 +0600

    added-guest-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-import PageNav from "./components/PageNav.jsx"
 import Error404 from "./pages/Error404.jsx"
 import Product from "./pages/Product.jsx"
 import Pricing from "./pages/Pricing.jsx"
+import Login from "./pages/Login.jsx"
 import Home from "./pages/Home.jsx"
 
 import React from "react"
@@ -12,11 +12,11 @@ import "./index.css"
 export default function App() {
     return (
         <BrowserRouter>
-            <PageNav />
             <Routes>
                 <Route path="/" exact element={<Home />} />
                 <Route path="/product" element={<Product />} />
                 <Route path="/pricing" element={<Pricing />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="*" element={<Error404 />} />
             </Routes>
         </BrowserRouter>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react"
+
+import styles from "./Login.module.css"
+import { Link } from "react-router-dom"
+import PageNav from "../components/PageNav.jsx"
+
+export default function Login() {
+    // PRE-FILL FOR DEV PURPOSES
+    const [email, setEmail] = useState("jack@example.com")
+    const [password, setPassword] = useState("qwerty")
+
+    return (
+        <main className={styles.login}>
+            <PageNav />
+            <form className={styles.form}>
+                <div className={styles.row}>
+                    <label htmlFor="email">Email address</label>
+                    <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+                </div>
+
+                <div className={styles.row}>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
+                </div>
+
+                <div>
+                    <Link to="%%%" type="button" className="cta">
+                        Login
+                    </Link>
+                </div>
+            </form>
+        </main>
+    )
+}
