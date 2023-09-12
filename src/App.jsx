84dbd0f1b43e1971eee5f14767e2e5c79a6df201
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-09-12 16:17:08 +0600

    added-base-routing-and-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Error404 from "./pages/Error404.jsx"
+import Product from "./pages/Product.jsx"
+import Pricing from "./pages/Pricing.jsx"
+import Home from "./pages/Home.jsx"
 import React from "react"
+import PageNav from "./components/PageNav.jsx"
 
 export default function App() {
-    const c = "dfdf"
-
     return (
-        <div>
-            <h1>WorldWise</h1>
-        </div>
+        <BrowserRouter>
+            <PageNav />
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/product" element={<Product />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="*" element={<Error404 />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
