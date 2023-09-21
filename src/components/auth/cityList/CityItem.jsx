fbc63bcb102d18a71b7bb81a232093b03ea64a6a
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-09-21 17:52:11 +0600

    added last visited city state

--- src/components/auth/cityList/CityItem.jsx
+++ src/components/auth/cityList/CityItem.jsx
@@ -2,12 +2,18 @@ import { formatDate } from "../../../utils/helpers.js"
 import styles from "./CityItem.module.css"
 import React from "react"
 import { Link } from "react-router-dom"
+import { useCitiesContext } from "../../../context/CitiesContext.jsx"
 
 export default function CityItem({ city }) {
+    const { currentCity } = useCitiesContext()
+
     const { emoji, cityName, date, position, id } = city
     return (
         <li>
-            <Link to={`${id}?lat=${position.lat}&lng=${position.lng}`} className={styles.cityItem}>
+            <Link
+                to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+                className={`${styles.cityItem} ${currentCity.id === id ? styles["cityItem--active"] : ""}`}
+            >
                 <span className={styles.emoji}>{emoji}</span>
                 <h3 className={styles.name}>{cityName}</h3>
                 <time className={styles.date}>({formatDate(date)})</time>
